8579ecebc97b3d3be68aaa205b4d2535c150d805
Derick Bailey <derickbailey@gmail.com>
2012-05-25 18:47:43 -0500

    added grunt build process

---
+++ grunt.js
@@ -0,0 +1,68 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-rigger');
+
+  // Project configuration.
+  grunt.initConfig({
+    meta: {
+      version: '0.0.1',
+      banner: '// Backbone.Picky, v<%= meta.version %>\n' +
+        '// Copyright (c)<%= grunt.template.today("yyyy") %> Derick Bailey, Muted Solutions, LLC.\n' + 
+        '// Distributed under MIT license\n' + 
+        '// http://github.com/derickbailey/backbone.picky'
+    },
+
+    lint: {
+      files: ['src/backbone.picky.js']
+    },
+
+    rig: {
+      build: {
+        src: ['<banner:meta.banner>', 'src/backbone.picky.js'],
+        dest: 'lib/backbone.picky.js'
+      },
+      amd: {
+        src: ['<banner:meta.banner>', 'src/amd.js'],
+        dest: 'lib/amd/backbone.picky.js'
+      }
+    },
+
+    min: {
+      standard: {
+        src: ['<banner:meta.banner>', '<config:rig.build.dest>'],
+        dest: 'lib/backbone.picky.min.js'
+      },
+      amd: {
+        src: ['<banner:meta.banner>', '<config:rig.amd.dest>'],
+        dest: 'lib/amd/backbone.picky.min.js'
+      }
+    },
+
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: false,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {
+        jQuery: true,
+        Backbone: true,
+        _: true
+      }
+    },
+    uglify: {}
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint rig min');
+
+};
