4304733831dc00edabc51c08e93d98f6d35a3f7c
Derick Bailey <derickbailey@gmail.com>
2012-05-26 23:26:52 -0500

    bump version to v0.1.0

--- grunt.js
+++ grunt.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     meta: {
-      version: '0.0.1',
+      version: '0.1.0',
       banner: '// Backbone.Picky, v<%= meta.version %>\n' +
         '// Copyright (c)<%= grunt.template.today("yyyy") %> Derick Bailey, Muted Solutions, LLC.\n' + 
         '// Distributed under MIT license\n' + 
