032bcb25d1624ae42c3dfa7409df690c6d255707
hashchange <heim@zeilenwechsel.de>
2013-12-30 17:24:16 +0100

    Added tests for deselectAll

--- spec/javascripts/multiSelect.deselect.spec.js
+++ spec/javascripts/multiSelect.deselect.spec.js
@@ -15,7 +15,7 @@ describe("multi-select collection deselecting", function(){
     }
   });
   
-  describe("when no models are selected, and deselecting all", function(){
+  describe("when no models are selected, and deselecting all (selectNone)", function(){
     var m1, m2, collection;
 
     beforeEach(function(){
@@ -42,7 +42,34 @@ describe("multi-select collection deselecting", function(){
     });
   });
 
-  describe("when 1 model is selected, and deselecting all", function(){
+  describe("when no models are selected, and deselecting all (deselectAll)", function(){
+    var m1, m2, collection;
+
+    beforeEach(function(){
+      m1 = new Model();
+      m2 = new Model();
+
+      collection = new Collection([m1, m2]);
+      spyOn(collection, "trigger").andCallThrough();
+
+      collection.deselectAll();
+    });
+
+    it("should trigger 'none' selected event", function(){
+      expect(collection.trigger).toHaveBeenCalledWith("select:none", collection);
+    });
+
+    it("should have a selected count of 0", function(){
+      expect(collection.selectedLength).toBe(0);
+    });
+
+    it("should not have any models in the selected list", function(){
+      var size = _.size(collection.selected);
+      expect(size).toBe(0);
+    });
+  });
+
+  describe("when 1 model is selected, and deselecting all (selectNone)", function(){
     var m1, m2, collection;
 
     beforeEach(function(){
@@ -70,7 +97,35 @@ describe("multi-select collection deselecting", function(){
     });
   });
 
-  describe("when all models are selected, and deselecting all", function(){
+  describe("when 1 model is selected, and deselecting all (deselectAll)", function(){
+    var m1, m2, collection;
+
+    beforeEach(function(){
+      m1 = new Model();
+      m2 = new Model();
+
+      collection = new Collection([m1, m2]);
+      m1.select();
+
+      spyOn(collection, "trigger").andCallThrough();
+      collection.deselectAll();
+    });
+
+    it("should trigger 'none' selected event", function(){
+      expect(collection.trigger).toHaveBeenCalledWith("select:none", collection);
+    });
+
+    it("should have a selected count of 0", function(){
+      expect(collection.selectedLength).toBe(0);
+    });
+
+    it("should not have any models in the selected list", function(){
+      var size = _.size(collection.selected);
+      expect(size).toBe(0);
+    });
+  });
+
+  describe("when all models are selected, and deselecting all (selectNone)", function(){
     var m1, m2, collection;
 
     beforeEach(function(){
@@ -85,6 +140,35 @@ describe("multi-select collection deselecting", function(){
       collection.selectNone();
     });
     
+    it("should trigger 'none' selected event", function(){
+      expect(collection.trigger).toHaveBeenCalledWith("select:none", collection);
+    });
+
+    it("should have a selected count of 0", function(){
+      expect(collection.selectedLength).toBe(0);
+    });
+
+    it("should not have any models in the selected list", function(){
+      var size = _.size(collection.selected);
+      expect(size).toBe(0);
+    });
+  });
+
+  describe("when all models are selected, and deselecting all (deselectAll)", function(){
+    var m1, m2, collection;
+
+    beforeEach(function(){
+      m1 = new Model();
+      m2 = new Model();
+
+      collection = new Collection([m1, m2]);
+      m1.select();
+      m2.select();
+
+      spyOn(collection, "trigger").andCallThrough();
+      collection.deselectAll();
+    });
+
     it("should trigger 'none' selected event", function(){
       expect(collection.trigger).toHaveBeenCalledWith("select:none", collection);
     });
